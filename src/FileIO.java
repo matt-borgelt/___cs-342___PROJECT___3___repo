3c7c651f97464b7aad16267ca1434bbfd7885552
mkborgelt13 <mkborgelt13@gmail.com>
2014-03-13 10:30:31 -0500

    v2
    Combined with TankMissile's alterations to Layout.java
    The entire input file is now stored internally

--- src/FileIO.java
+++ src/FileIO.java
@@ -3,13 +3,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileIO {
 	
 	
 	public FileIO(){
-		
+		ArrayList<String> stringList = new ArrayList<String>();
+
 		String line;
 		try{
 			File f = new File("level1.txt");
@@ -17,10 +19,9 @@ public class FileIO {
 			int i=0;
 			while((line = inFile.readLine()) != null){
 				//System.out.println(line);
-				line.trim();
+				//line.trim();
 				if ( (i == 0) && (line.charAt(1)==' ') ){
 					/* the header of the input file --  rows x columns */
-					
 					Layout.setRows(  Character.getNumericValue(line.charAt(0)) );
 					Layout.setCols(  Character.getNumericValue(line.charAt(3)) );
 		
@@ -28,12 +29,15 @@ public class FileIO {
 				else{
 					/* input file body */
 					System.out.println(line);
+					stringList.add(line);
+					
 				}
 				i++;
 			}
+			Layout.setBody(stringList);
 			inFile.close();
 		}catch(IOException e){
 			System.err.println("File not found... :(");
-		}	
+		}
 	}
 }
--- src/Layout.java
+++ src/Layout.java
@@ -4,17 +4,18 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.Color;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Layout {
 
-	//public static final int WIDTH = 300;
-	//public static final int HEIGHT = 300;
-
 	public static int rows;
 	public static int cols;
-
+    public static ArrayList<String> list = new ArrayList<String>();
+    
+    /* "Getters" and "Setters"
+     * This allows the secure transfer of info from the read-in file between FileIO and Layout classes */
 	public static void setRows(int data){
 		rows = data;
 	}
@@ -28,9 +29,22 @@ public class Layout {
 	public static int getCols(){
 		return cols;
 	}
+	
+	public static void setBody( ArrayList<String> data){
+		list = data;
+	}
+	public static ArrayList<String> getBody(){
+		return list;
+	}
 
 	public Layout()
 	{
+		FileIO io = new FileIO();
+		System.out.println("# of Rows: " + getRows() );
+		System.out.println("# of Cols: " + getCols() );
+		System.out.println("Body is: \n" + getBody() );
+		
+		
 		JFrame frame = new JFrame("Slide Puzzle");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -40,14 +54,14 @@ public class Layout {
 		tile1.setPreferredSize(new Dimension(100,50));
 
 
-		System.out.println("# of Rows: " + getRows() );
-		System.out.println("# of Cols: " + getCols() );
+		
 
 		JButton btn_solve = new JButton("Solve");
 		btn_solve.setSize(70,30);
-		tile1.add(btn_solve, BorderLayout.CENTER);
+		frame.add(btn_solve, BorderLayout.CENTER);
 		btn_solve.setVisible(true);
 		
+		frame.setResizable(false);
 		frame.getContentPane().add(tile1, BorderLayout.NORTH);
 		frame.pack();
 		frame.setVisible(true);
