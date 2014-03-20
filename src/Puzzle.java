d15e08a75584b75cbf737a1e6a4e2a13a5c270cb
TankMissile <TankMissile@users.noreply.github.com>
2014-03-19 20:53:55 -0500

    Update Puzzle.java

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,11 +1,10 @@
-
 public class Puzzle {
 	public static void main(String args[])
 	{
 		/* FOR 1..12 initialize a new layout with a new input file... for the different levels 
 		 * WHEN level is DONE iterate */
-		
-		Layout L = new Layout();
+
+		Layout L = new Layout(1); //pass a number corresponding to the level number to be loaded, i.e. for "level1.txt" pass the value "1"
 	}
 
 }
